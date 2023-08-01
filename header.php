0635ad36c27205e0243898b3d85d34caa72b468e
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-08-01 22:42:08 +0530

    basic setup

--- header.php
+++ header.php
@@ -5,7 +5,36 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
         <link href="./assests/header-style.css" rel="stylesheet">
+        <link href="./assests/style.css" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
 
     </head>
 
     <body>
+
+    <header>
+    <div class="container">
+        <div class="row">
+            <div class="col-2" style="width: 20%;">
+                <!-- Site Logo -->
+                <img src="./assests/images/site-logo.png" alt="Logo" class="site-logo">
+            </div>
+            <div class="col-4" style="width: 40%;">
+                <!-- Search Box -->
+                <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <div class="col-2" style="width: 20%;">
+                <!-- Contact Number -->
+                <p>Inquiries and Support  </br>+1234567890</p>
+            </div>
+            <div class="col-2" style="width: 20%; display: flex; gap: 50px;">
+                <!-- Icon Group (User, Wishlist, Cart) -->
+                <i class="fas fa-user"></i>
+                <i class="fas fa-heart"></i>
+                <i class="fas fa-shopping-cart"></i>
+            </div>
+        </div>
+    </div>
