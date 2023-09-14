3b33fb39e65b5ee152df199d689413fcb5594a67
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-09-14 14:00:16 +0530

    Banners

--- index.php
+++ index.php
@@ -1,68 +1,8 @@
 <?php include('header.php'); ?>
 
 <main>
-    <div class="main-content">
-        <div class="container">
-            <div class="row">
-                <div class="flex3">
-                <div id="slideshow" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
-    <div class="carousel-inner">
 
-        <!-- Slide 1 -->
-        <div class="carousel-item active slide-item" style="background-image: url('./assests/images/gng-slide-1.png');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-lg-4 slide-content">
-                        <div class="inner-content">
-                            <h2>Discover New Amazing </br>Grocery Deals</h2>
-                            <p>The most Healty organic </br>Food is save for health</p>
-                            <button class="btn btn-primary">Shop Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Slide 2 (Add more slides as needed) -->
-        <div class="carousel-item slide-item" style="background-image: url('./assests/images/gng-slide-2.png');">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-lg-4 slide-content">
-                        <div class="inner-content">
-                            <h2>Discover New </br>Electronic Deals</h2>
-                            <p>The latest electronic items </br>for afordable prices</p>
-                            <button class="btn btn-primary">Shop Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Add more slides as needed -->
-
-    </div>
-    <ol class="carousel-indicators">
-        <li data-bs-target="#slideshow" data-bs-slide-to="0" class="active"></li>
-        <li data-bs-target="#slideshow" data-bs-slide-to="1"></li>
-        <!-- Add more navigation dots as needed -->
-    </ol>
-</div>
-                </div>
-                <div class="flex2">
-                    <div class="hero-promo" style="background-image: url('./assests/images/hero-image.png');">
-                        <div class="inner-content-hero">
-                            <span class="banner-span">Big Sale</span>
-                            <h2 class="banner-h2">Asus ROG Laptop</h2>
-                            <button class="btn btn-primary banner-button">15% OFF</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
+    <?php include('./sections/banners.php'); ?>
     <?php include('./sections/category-slider.php'); ?>
     <?php include('./sections/product-slider.php'); ?>
     <?php include('./sections/hot-offers.php'); ?>
---
+++ sections/banners.php
@@ -0,0 +1,126 @@
+<!--Home Slideshow-->
+<section class="slideshow slideshow-wrapper">
+    <div class="container container-1330">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-8">
+                <div class="home-slideshow slideshow-medium slick-arrow-dots circle-arrow">
+                    <div class="slide">
+                        <div class="slideshow-wrap bg-size rounded-4">
+                            <img class="bg-img rounded-4 blur-up lazyload"
+                                data-src="assests/images/slideshow/demo7-banner1.jpg"
+                                src="assests/images/slideshow/demo7-banner1.jpg" alt="slideshow" title="" width="1148"
+                                height="710" />
+                            <div class="slideshow-content slideshow-overlay middle-center">
+                                <!-- <div class="slideshow-content-in">
+                                    <div class="wrap-caption animation style3">
+                                        <p class="ss-small-title">Best Selling</p>
+                                        <h2 class="ss-mega-title">100% Organic Foods</h2>
+                                        <p class="ss-sub-title xs-hide">The more you love your health, more you eat
+                                            organic. <br />No medicine can heal you better than organic food does</p>
+                                        <div class="ss-btnWrap">
+                                            <a class="btn btn-primary rounded-pill" href="shop-grid-view.html">Order
+                                                Now</a>
+                                            <a class="btn btn-primary rounded-pill" href="shop-grid-view.html">Explore
+                                                Now</a>
+                                        </div>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <div class="slideshow-wrap bg-size rounded-4">
+                            <img class="bg-img rounded-4 blur-up lazyload"
+                                data-src="assests/images/slideshow/demo7-banner2.jpg"
+                                src="assests/images/slideshow/demo7-banner2.jpg" alt="slideshow" title="" width="1148"
+                                height="710" />
+                            <div class="slideshow-content slideshow-overlay middle-left">
+                                <!-- <div class="slideshow-content-in">
+                                    <div class="wrap-caption animation style3">
+                                        <p class="ss-small-title">Fresh Vegetables</p>
+                                        <h2 class="ss-mega-title">100% Natural <br>Organic</h2>
+                                        <p class="ss-sub-title">Save up to 50% off on your first order</p>
+                                        <div class="ss-btnWrap">
+                                            <a class="btn btn-primary rounded-pill" href="shop-grid-view.html">Shop
+                                                Collection</a>
+                                        </div>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slide">
+                        <div class="slideshow-wrap bg-size rounded-4">
+                            <img class="bg-img rounded-4 blur-up lazyload"
+                                data-src="assests/images/slideshow/demo7-banner3.jpg"
+                                src="assests/images/slideshow/demo7-banner3.jpg" alt="slideshow" title="" width="1148"
+                                height="710" />
+                            <div class="slideshow-content slideshow-overlay middle-right">
+                                <!-- <div class="slideshow-content-in">
+                                    <div class="wrap-caption animation style3">
+                                        <p class="ss-small-title">Best Sellers</p>
+                                        <h2 class="ss-mega-title">The best Organic <br />Products Online</h2>
+                                        <p class="ss-sub-title">New Launched Let's Try Our Latest Upcoming</p>
+                                        <div class="ss-btnWrap">
+                                            <a class="btn btn-primary rounded-pill" href="shop-grid-view.html">Shop
+                                                Collection</a>
+                                        </div>
+                                    </div>
+                                </div> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+                <div class="collection-banner-grid">
+                    <div class="row sp-row">
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-12 collection-banner-item">
+                            <div class="collection-item sp-col">
+                                <a href="shop-left-sidebar.html" class="rounded-4 zoom-scal clr-none">
+                                    <div class="img">
+                                        <img class="rounded-4 w-100 blur-up lazyload"
+                                            data-src="assests/images/slideshow/demo7-small-banner1.jpg"
+                                            src="assests/images/slideshow/demo7-small-banner1.jpg" alt="collection"
+                                            title="" width="454" height="268" />
+                                    </div>
+                                    <div class="details middle-left">
+                                        <!-- <div class="inner">
+                                            <span class="small-title mb-2 text-muted d-block alt-font">Vegetables</span>
+                                            <h3 class="title">Fresh & Clean</h3>
+                                            <span class="btn btn-outline-secondary rounded-pill mt-3 btn-small">Shop
+                                                Now</span>
+                                        </div> -->
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-12 col-md-6 col-lg-12 collection-banner-item">
+                            <div class="collection-item sp-col">
+                                <a href="shop-left-sidebar.html" class="rounded-4 zoom-scal clr-none">
+                                    <div class="img">
+                                        <img class="rounded-4 w-100 blur-up lazyload"
+                                            data-src="assests/images/slideshow/demo7-small-banner2.jpg"
+                                            src="assests/images/slideshow/demo7-small-banner2.jpg" alt="collection"
+                                            title="" width="454" height="268" />
+                                    </div>
+                                    <div class="details middle-left">
+                                        <!-- <div class="inner">
+                                            <span class="small-title mb-2 alt-font text-muted d-block">Popular
+                                                Foods</span>
+                                            <h3 class="title">Everyday Fresh &amp; <br />Clean with Our <br />Products
+                                            </h3>
+                                            <span class="btn btn-outline-secondary rounded-pill mt-3 btn-small">Shop
+                                                Now</span>
+                                        </div> -->
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End Home Slideshow-->
