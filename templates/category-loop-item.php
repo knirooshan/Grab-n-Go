bd6d02da284768db514a219ae3376d7e670cce52
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-09-15 14:22:44 +0530

    New Category Slider

---
+++ templates/category-loop-item.php
@@ -0,0 +1,11 @@
+<div class="category-item zoomscal-hov rounded-4">
+    <a href="" class="category-link clr-none" style="background-color: <?php echo $bgcolor; ?>;">
+        <div class="zoom-scal zoom-scal-nopb rounded-circle"><img class="rounded-circle blur-up lazyload"
+                data-src="assests/images/collection/demo7-collection2.png"
+                src="assests/images/collection/demo7-collection2.png" alt="collection" title="" width="211"
+                height="211" /></div>
+        <div class="details mt-3 pt-1 text-center">
+            <h4 class="category-title mb-0 body-font">Fresh Fruits</h4>
+        </div>
+    </a>
+</div>
