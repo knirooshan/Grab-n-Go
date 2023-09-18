fd9eb8c20c808466af9ee4fce1257598e905ec48
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-09-18 11:34:52 +0530

    Category Loop Item for Shop

---
+++ templates/shop-category-item.php
@@ -0,0 +1,12 @@
+<div class="category-item zoomscal-hov">
+    <a href="" class="category-link clr-none">
+        <div class="zoom-scal zoom-scal-nopb rounded-0"><img class="rounded-0 blur-up lazyload"
+                data-src="assests/images/collection/sub-collection16.jpg"
+                src="assests/images/collection/sub-collection16.jpg" alt="Men's" title="Men's" width="365"
+                height="365" /></div>
+        <div class="details text-center">
+            <h4 class="category-title mb-0">Furniture</h4>
+            <p class="counts">20 Items</p>
+        </div>
+    </a>
+</div>
