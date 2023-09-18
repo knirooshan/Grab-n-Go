9d639f3b0934930588dc5bd8f5232af0aeb5d8e0
Kaveesha Nirooshan <Kaveeshanirooshan2003@gmail.com>
2023-09-18 11:35:29 +0530

    Product Loop Item for Shop

---
+++ templates/shop-product-item.php
@@ -0,0 +1,82 @@
+<div class="item col-item">
+    <div class="product-box">
+        <!-- Start Product Image -->
+        <div class="product-image">
+            <!-- Start Product Image -->
+            <a href="" class="product-img rounded-0"><img class="rounded-0 blur-up lazyload" src="assests/images/products/product1.jpg" alt="Product" title="Product" width="625" height="808" /></a>
+            <!-- End Product Image -->
+            <!-- Product label -->
+            <div class="product-labels"><span class="lbl on-sale">Trending</span></div>
+            <!-- End Product label -->
+            <!--Countdown Timer-->
+            <!-- <div class="saleTime" data-countdown="2025/01/01"></div> -->
+            <!--End Countdown Timer-->
+            <!--Product Button-->
+            <div class="button-set style1">
+                <!--Cart Button-->
+                <a href="#quickshop-modal" class="btn-icon addtocart quick-shop-modal" data-bs-toggle="modal" data-bs-target="#quickshop_modal">
+                    <span class="icon-wrap d-flex-justify-center h-100 w-100" data-bs-toggle="tooltip" data-bs-placement="left" title="Quick Shop"><i class="icon anm anm-cart-l"></i><span class="text">Quick Shop</span></span>
+                </a>
+                <!--End Cart Button-->
+                <!--Quick View Button-->
+                <a href="#quickview-modal" class="btn-icon quickview quick-view-modal" data-bs-toggle="modal" data-bs-target="#quickview_modal">
+                    <span class="icon-wrap d-flex-justify-center h-100 w-100" data-bs-toggle="tooltip" data-bs-placement="left" title="Quick View"><i class="icon anm anm-search-plus-l"></i><span class="text">Quick View</span></span>
+                </a>
+                <!--End Quick View Button-->
+                <!--Wishlist Button-->
+                <a href="" class="btn-icon wishlist" data-bs-toggle="tooltip" data-bs-placement="left" title="Add To Wishlist"><i class="icon anm anm-heart-l"></i><span class="text">Add To Wishlist</span></a>
+                <!--End Wishlist Button-->
+                <!--Compare Button-->
+                <a href="" class="btn-icon compare" data-bs-toggle="tooltip" data-bs-placement="left" title="Add to Compare"><i class="icon anm anm-random-r"></i><span class="text">Add to Compare</span></a>
+                <!--End Compare Button-->
+            </div>
+            <!--End Product Button-->
+        </div>
+        <!-- End Product Image -->
+        <!-- Start Product Details -->
+        <div class="product-details text-center">
+            <!--Product Vendor-->
+            <div class="product-vendor">Tops</div>
+            <!--End Product Vendor-->
+            <!-- Product Name -->
+            <div class="product-name">
+                <a href="">Oxford Cuban Shirt</a>
+            </div>
+            <!-- End Product Name -->
+            <!-- Product Price -->
+            <div class="product-price">
+                <span class="price old-price">LKR 114.00</span><span class="price">LKR 99.00</span>
+            </div>
+            <!-- End Product Price -->
+            <!-- Product Review -->
+            <div class="product-review">
+                <i class="icon anm anm-star"></i><i class="icon anm anm-star"></i><i class="icon anm anm-star"></i><i class="icon anm anm-star"></i><i class="icon anm anm-star-o"></i>
+                <span class="caption hidden ms-1">3 Reviews</span>
+            </div>
+            <!-- End Product Review -->
+            <!--Sort Description-->
+            <p class="sort-desc hidden">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage...</p>
+            <!--End Sort Description-->
+            <!--Color Variant -->
+            <ul class="variants-clr swatches">
+                <li class="swatch medium radius"><span class="swatchLbl" data-bs-toggle="tooltip" data-bs-placement="top" title="Navy"><img src="assests/images/products/product1.jpg" alt="img" width="625" height="808"></span></li>
+                <li class="swatch medium radius"><span class="swatchLbl" data-bs-toggle="tooltip" data-bs-placement="top" title="Green"><img src="assests/images/products/product1-1.jpg" alt="img" width="625" height="808"></span></li>
+                <li class="swatch medium radius"><span class="swatchLbl" data-bs-toggle="tooltip" data-bs-placement="top" title="Gray"><img src="assests/images/products/product1-2.jpg" alt="img" width="625" height="808"></span></li>
+                <li class="swatch medium radius"><span class="swatchLbl" data-bs-toggle="tooltip" data-bs-placement="top" title="Orange"><img src="assests/images/products/product1-3.jpg" alt="img" width="625" height="808"></span></li>
+            </ul>
+            <!-- End Variant -->
+            <!-- Product Button -->
+            <div class="button-action hidden">
+                <div class="addtocart-btn">
+                    <form class="addtocart" action="#" method="post">
+                        <a href="#quickshop-modal" class="btn btn-md quick-shop quick-shop-modal" data-bs-toggle="modal" data-bs-target="#quickshop_modal">
+                            <i class="icon anm anm-cart-l me-2"></i><span class="text">Quick Shop</span>
+                        </a>
+                    </form>
+                </div>
+            </div>
+            <!-- End Product Button -->
+        </div>
+        <!-- End product details -->
+    </div>
+</div>
